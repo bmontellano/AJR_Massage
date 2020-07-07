406597de02783997d2d43907ea3595728e768de8
Michelle Hinojosa <michellehinojosa@Michelles-MacBook.local>
2020-07-07 10:21:54 -0700

    adding button disable

---
+++ index.js
@@ -0,0 +1,34 @@
+// INIT Jacvascript
+//getElementsByTagName creates collection
+var firstButton = document.querySelector('ion-button');
+var siteTitleElement = document.getElementById('site-title');
+var counter = 0;
+
+//clicking button adds element and removes it
+firstButton.addEventListener('click', () => {
+  if (counter = 0) {
+    //current time
+    var currentTime = new Date();
+    var milliTime = currentTime.getTime();
+    var seconds = milliTime/1000;
+  }
+  counter ++;
+  //disable button after click
+  firstButton.setAttribute("disabled","true");
+
+  console.log('clicked at ' + currentTime + "\nepochTime: " + milliTime + "\ncounter:" + counter);
+  var newDiv = document.createElement('div');
+  newDiv.style.cssText =  'text-align:center; color:#de9918';
+  var newContent = document.createTextNode("Appointment made!");
+  newDiv.appendChild(newContent);
+  //append after title
+  document.body.insertBefore(newDiv,siteTitleElement.nextSibling);
+  //remove child after 3 secs
+  setTimeout(() => {
+    newDiv.parentNode.removeChild(newDiv);
+  },3000);
+  setTimeout(() => {
+    counter = 0;
+    firstButton.setAttribute("disabled","false");
+  }, 6000);
+});
